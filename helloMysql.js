65a0ff60efef832c2c218417983bbc1b6f8b38f9
bartleke <bartleke@oregonstate.edu>
2016-06-01 01:49:21 -0700

    asd

--- helloMysql.js
+++ helloMysql.js
@@ -23,7 +23,8 @@ app.get('/',function(req,res,next){
 
 app.get('/insert',function(req,res,next){
   var context = {};
-  mysql.pool.query("INSERT INTO workout (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", [req.query.name,req.query.reps,req.query.weight,req.query.date,req.query.lbs], function(err, result){
+  mysql.pool.query("INSERT INTO workout (`name`,`reps`,`weight`,`date`,`lbs`) VALUES (?,?,?,?,?)", 
+  [req.query.name,req.query.reps,req.query.weight,req.query.date,req.query.lbs], function(err, result){
     if(err){
       next(err);
       return;
@@ -50,7 +51,7 @@ app.get('/delete',function(req,res,next){
 app.get('/simple-update',function(req,res,next){
   var context = {};
   mysql.pool.query("UPDATE workout SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
-       [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.date || curVals.date, req.query.lbs || curVals.lbs, req.query.id],
+        [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs, req.query.id],
        function(err, result){
     if(err){
       next(err);
@@ -72,7 +73,7 @@ app.get('/safe-update',function(req,res,next){
     if(result.length == 1){
       var curVals = result[0];
       mysql.pool.query("UPDATE workout SET name=?, reps=?, weight=?, date=?, lbs=? WHERE id=? ",
-       [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.date || curVals.date, req.query.lbs || curVals.lbs, req.query.id],
+	   [req.query.name || curVals.name, req.query.reps || curVals.reps, req.query.weight || curVals.weight, req.query.date || curVals.date, req.query.lbs || curVals.lbs, req.query.id],
         function(err, result){
         if(err){
           next(err);
