80a6469f56d08950358110f37d88a7d6bfdfece7
bartleke <bartleke@oregonstate.edu>
2016-06-01 03:32:21 -0700

    b

---
+++ buttons.js
@@ -0,0 +1,33 @@
+/*document.getElementById('AddEntry').addEventListener('click', function(event){
+    var req = new XMLHttpRequest();
+	var payload = {};
+   	payload.id = this.parentNode.name;
+    req.open('POST', 'http://localhost:3000/insert', true);
+	req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load',function(){
+      if(req.status >= 200 && req.status < 400){
+        var response = JSON.parse(req.result);
+        console.log(result);
+      } else {
+        console.log("Error in network request: " + request.statusText);
+      }});
+    req.send(null);
+    event.preventDefault();
+  });
+  
+document.getElementById('deleteb').addEventListener('click', function(event){
+    var req = new XMLHttpRequest();
+	var payload = {};
+   	payload.id = this.parentNode.name;
+    req.open('POST', 'http://localhost:3000/delete', true);
+	req.setRequestHeader('Content-Type', 'application/json');
+    req.addEventListener('load',function(){
+      if(req.status >= 200 && req.status < 400){
+        var response = JSON.parse(req.result);
+        console.log(result);
+      } else {
+        console.log("Error in network request: " + request.statusText);
+      }});
+    req.send(null);
+    event.preventDefault();
+  });
--- helloMysql.js
+++ helloMysql.js
@@ -151,37 +151,3 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
   console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
-
-/*document.getElementById('AddEntry').addEventListener('click', function(event){
-    var req = new XMLHttpRequest();
-	var payload = {};
-   	payload.id = this.parentNode.name;
-    req.open('POST', 'http://localhost:3000/insert', true);
-	req.setRequestHeader('Content-Type', 'application/json');
-    req.addEventListener('load',function(){
-      if(req.status >= 200 && req.status < 400){
-        var response = JSON.parse(req.result);
-        console.log(result);
-      } else {
-        console.log("Error in network request: " + request.statusText);
-      }});
-    req.send(null);
-    event.preventDefault();
-  });
-  
-document.getElementById('deleteb').addEventListener('click', function(event){
-    var req = new XMLHttpRequest();
-	var payload = {};
-   	payload.id = this.parentNode.name;
-    req.open('POST', 'http://localhost:3000/delete', true);
-	req.setRequestHeader('Content-Type', 'application/json');
-    req.addEventListener('load',function(){
-      if(req.status >= 200 && req.status < 400){
-        var response = JSON.parse(req.result);
-        console.log(result);
-      } else {
-        console.log("Error in network request: " + request.statusText);
-      }});
-    req.send(null);
-    event.preventDefault();
-  });
